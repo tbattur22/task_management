d2d355b282ced7e8d44266e975804f1a33c244aa
Battur Tugsgerel <batturtugsgerel@MacBookPro.attlocal.net>
2025-06-03 11:30:34 -0500

    Using psql instead of ps_isready for checking db ready status, not seeding db.

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 # Wait until Postgres is ready
-until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USERNAME"; do
+until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USERNAME" -d "$DB_DATABASE" -c '\q' 2>/dev/null; do
   echo "⏳ Waiting for Postgres at $DB_HOST:$DB_PORT..."
   sleep 2
 done
 
 echo "✅ Postgres is ready — running migrations..."
 php artisan migrate --force
-php artisan db:seed --force
 
 # Start Apache
 exec apache2-foreground
